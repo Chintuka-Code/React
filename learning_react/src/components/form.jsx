3be18bedab902420d18a777c5953f561e9110ccb
mohmad sabban <mohmadsabban123@gmail.com>
2020-07-20 00:54:26 +0530

    Multiple_Input_Form

--- learning_react/src/components/form.jsx
+++ learning_react/src/components/form.jsx
@@ -3,25 +3,46 @@ import React, { Component } from 'react';
 class form extends Component {
   constructor(props) {
     super(props);
-    this.state = { value: '' };
+    this.state = { name: '', age: '', message: '' };
   }
 
   changehandler(e) {
-    this.setState({ value: e.target.value });
+    const name = e.target.name;
+    const value = e.target.value;
+    this.setState({ [name]: value });
   }
 
   getname(e) {
     e.preventDefault();
-    console.log(this.state.value);
+    console.log(e.target[1].name);
+    this.setState({ message: 'not ok' });
   }
 
   render() {
     return (
+      //   <form onSubmit={this.getname.bind(this)}>
+      //     <input
+      //       type="text"
+      //       onChange={this.changehandler.bind(this)}
+      //       name=""
+      //       id=""
+      //     />
+      //     <button type="submit">Get Name</button>
+      //   </form>
+
+      //   Multiple input
       <form onSubmit={this.getname.bind(this)}>
         <input
           type="text"
           onChange={this.changehandler.bind(this)}
-          name=""
+          name="name"
+          id=""
+        />
+        <h1>{this.state.message}</h1>
+        <input
+          type="text"
+          onChange={this.changehandler.bind(this)}
+          name="age"
           id=""
         />
         <button type="submit">Get Name</button>
